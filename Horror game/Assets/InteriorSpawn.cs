df11b9392a69f4a8c677d5582b723c4f3c4ce766
Shnap! <143894257+Shnap1@users.noreply.github.com>
2025-01-02 19:00:32 +0300

    random spawn bug FIXED! + added floor spawn

---
+++ Horror game/Assets/InteriorSpawn.cs
@@ -0,0 +1,87 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteriorSpawn : MonoBehaviour
+{
+    public List<GameObject> Floor_Placeholders = new List<GameObject>();
+    public List<GameObject> Walls_Placeholders = new List<GameObject>();
+    // public List<GameObject> L_Placeholders = new List<GameObject>();
+
+    public List<GameObject> Floors = new List<GameObject>();
+    public List<GameObject> Walls = new List<GameObject>();
+    public List<GameObject> Guests = new List<GameObject>();
+    // public List<GameObject> L_Buildings = new List<GameObject>();
+    void Start()
+    {
+        SpawnAllSectorInteriors();
+
+        ShowInterior(false, Floor_Placeholders);
+        ShowInterior(false, Walls_Placeholders);
+    }
+
+    void Update()
+    {
+        if (Input.GetKey("r"))
+        {
+            SpawnAllSectorInteriors();
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player" || other.gameObject.tag == "Enemy")
+        {
+            Guests.Add(other.gameObject);
+            ShowInterior(true, Floor_Placeholders);
+            ShowInterior(true, Walls_Placeholders);
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player" || other.gameObject.tag == "Enemy")
+            Guests.Remove(other.gameObject);
+        if (Guests.Count == 0)
+        {
+            ShowInterior(false, Floor_Placeholders);
+            ShowInterior(false, Walls_Placeholders);
+        }
+    }
+
+
+
+    public void SpawnAllSectorInteriors()
+    {
+        SpawnRandomBuildings(Floor_Placeholders, Floors);
+        SpawnRandomBuildings(Walls_Placeholders, Walls);
+        // SpawnRandomBuildings(L_Placeholders, L_Buildings);
+    }
+
+    public void SpawnRandomBuildings(List<GameObject> placeholders, List<GameObject> Parts)
+    {
+        if (placeholders.Count > 0)
+        {
+            for (int i = 0; i < placeholders.Count; i++)
+            {
+                GameObject randomBuilding = Parts[Random.Range(0, Parts.Count)];
+                var instantiatedBuilding = Instantiate(randomBuilding, placeholders[i].transform.position, placeholders[i].transform.rotation);
+
+                if (placeholders[i] != null) Destroy(placeholders[i].gameObject);
+
+                placeholders[i] = instantiatedBuilding;
+            }
+        }
+        else
+        {
+            Debug.Log("No more placeholders in" + nameof(placeholders));
+        }
+    }
+
+    public void ShowInterior(bool show, List<GameObject> placeholders)
+    {
+        for (int i = 0; i < placeholders.Count; i++)
+        {
+            placeholders[i].gameObject.SetActive(show);
+        }
+    }
+}
