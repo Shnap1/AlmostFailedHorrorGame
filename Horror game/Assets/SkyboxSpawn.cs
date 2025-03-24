39a135bed2cbc6b7d3da0ad99a68cb0f6c17505f
Shnap! <143894257+Shnap1@users.noreply.github.com>
2025-03-25 01:18:25 +0300

    19 new sky boxes textures + script for random skyboxes. + bloom effect changed

---
+++ Horror game/Assets/SkyboxSpawn.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+
+public class SkyboxSpawn : MonoBehaviour
+{
+    [SerializeField] private List<Texture> cubeMapList;
+
+    private void Start()
+    {
+
+        SetRandomSkybox();
+    }
+
+
+    private void SetRandomSkybox()
+    {
+        int randomIndex = Random.Range(0, cubeMapList.Count);
+        ChangeSkybox(cubeMapList[randomIndex]);
+    }
+
+    private void ChangeSkybox(Texture newTexture)
+    {
+        ((Material)RenderSettings.skybox).mainTexture = newTexture;
+
+        Material skyboxMaterial = new Material(Shader.Find("Skybox/Cubemap"));
+        skyboxMaterial.SetTexture("_Tex", newTexture);
+
+        // Assign the material to the scene's skybox
+        RenderSettings.skybox = skyboxMaterial;
+
+        // Update the environment lighting if using dynamic GI
+        DynamicGI.UpdateEnvironment();
+
+    }
+
+
+
+}
+
+
