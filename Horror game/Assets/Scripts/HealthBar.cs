7ffc064d544c289e883f45457543253fcbae577e
Shnap! <143894257+Shnap1@users.noreply.github.com>
2025-05-12 22:18:11 +0300

    deketed unused switch

--- Horror game/Assets/Scripts/HealthBar.cs
+++ Horror game/Assets/Scripts/HealthBar.cs
@@ -4,7 +4,6 @@ using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.UI;
 using YG;
-//using UnityEngine.UIElements;
 
 public class HealthBar : MonoBehaviour
 {
@@ -78,69 +77,9 @@ public class HealthBar : MonoBehaviour
         this.number_of_TARGETS_to_collect = number_of_TARGETS_to_collect;
         this.number_of_TARGETS_collected = number_of_TARGETS_collected;
 
-        UpdateStateText(currentGameState, number_of_TARGETS_to_collect, number_of_TARGETS_collected);
     }
 
-    void UpdateStateText(GameLoopManager.GameState gameState, int number_of_TARGETS_to_collect, int number_of_TARGETS_collected)
-    {
-        //currentGameStateText.text = gameState.ToString();
-        // Debug.Log($"UpdateStateText() gameState = {gameState} + curLanguage =  {curLanguage} ");
 
-        switch (gameState)
-        {
-            // case GameLoopManager.GameState.SpawnPlayer:
-
-            //     if (curLanguage == GameLanguages.en)
-            //     {
-            //         currentGameStateText.text = "Go beyond the safe zone.";
-            //     }
-            //     else if (curLanguage == GameLanguages.ru)
-            //     {
-            //         currentGameStateText.text = "Выйди за границы безопасной зоны.";
-            //     }
-            //     else if (curLanguage == GameLanguages.tr)
-            //     {
-            //         currentGameStateText.text = "Güvenli bölgenin ötesine geçin.";
-            //     }
-            //     break;
-
-            // case GameLoopManager.GameState.GameStart:
-            //     if (curLanguage == GameLanguages.en)
-            //     {
-            //         currentGameStateText.text = $"{gameLoopManager.current_number_of_TARGETS_collected} of  {gameLoopManager.number_of_TARGETS_to_collect} monsters killed. Kill all monsters.";
-            //     }
-            //     else if (curLanguage == GameLanguages.ru)
-            //     {
-            //         currentGameStateText.text = $"{gameLoopManager.current_number_of_TARGETS_collected} из {gameLoopManager.number_of_TARGETS_to_collect} монстров убито. Убей всех монстров.";
-            //     }
-            //     else if (curLanguage == GameLanguages.tr)
-            //     {
-            //         currentGameStateText.text = $"Öldürülen {gameLoopManager.number_of_TARGETS_to_collect} canavardan {gameLoopManager.current_number_of_TARGETS_collected}'ı. Bütün canavarları öldür.";
-            //     }
-            //     break;
-            // case GameLoopManager.GameState.LootCollected:
-            //     if (curLanguage == GameLanguages.en)
-            //     {
-            //         currentGameStateText.text = "Mission accomplished. Return to the safe zone.";
-            //     }
-            //     else if (curLanguage == GameLanguages.ru)
-            //     {
-            //         currentGameStateText.text = "Задание выполнено. Возвращайтесь в безопасную зону.";
-            //     }
-            //     else if (curLanguage == GameLanguages.tr)
-            //     {
-            //         currentGameStateText.text = "Görev tamamlandı. Güvenli bölgeye dönün.";
-            //     }
-            //     break;
-            // case GameLoopManager.GameState.Lobby:
-            //     currentGameStateText.text = "You are now in the lobby. Go to the gates to start the game";
-            //     break;
-            // default:
-            //     currentGameStateText.text = "";
-
-            //     break;
-        }
-    }
 
     void UpdateHealthUI(int playerHealth, int playerHealthMax)
     {
@@ -148,9 +87,5 @@ public class HealthBar : MonoBehaviour
         healthSlider.maxValue = playerHealthMax;
     }
 
-    //private void Start()
-    //{
-    //    healthSlider = GetComponentInChildren<Slider>();
-    //    UpdateHealthUI(100,100);
-    //}
+
 }
