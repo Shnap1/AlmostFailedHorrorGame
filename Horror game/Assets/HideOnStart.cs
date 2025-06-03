05891a9d04ab20754a1d2fae68651c0ae562f1f0
Shnap! <143894257+Shnap1@users.noreply.github.com>
2025-06-03 17:27:46 +0300

    BUG FIXED jumping pad not spawning where intended

--- Horror game/Assets/HideOnStart.cs
+++ Horror game/Assets/HideOnStart.cs
@@ -4,16 +4,11 @@ using UnityEngine;
 
 public class HideOnStart : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
         var meshRenderer = gameObject.GetComponent<MeshRenderer>();
         meshRenderer.enabled = false;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
 
-    }
 }
