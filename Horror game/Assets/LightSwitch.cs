844ffa8984b41d43a150a19dca32b3fb52cd4a2c
Shnap! <143894257+Shnap1@users.noreply.github.com>
2025-06-03 17:11:00 +0300

    +light toggle added

---
+++ Horror game/Assets/LightSwitch.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightSwitch : MonoBehaviour
+{
+    Light playerFlashlight;
+    public bool FlashlightOn = true;
+    void Start()
+    {
+        playerFlashlight = gameObject.GetComponent<Light>();
+
+    }
+
+    public void ToggleFlashlight()
+    {
+        if (FlashlightOn)
+        {
+            playerFlashlight.enabled = false;
+            FlashlightOn = false;
+        }
+        else
+        {
+            playerFlashlight.enabled = true;
+            FlashlightOn = true;
+        }
+    }
+
+    public void FlashlightSwitch(bool FlashlightOn)
+    {
+        if (FlashlightOn == true)
+        {
+            playerFlashlight.enabled = true;
+        }
+        else if (FlashlightOn == false)
+        {
+            playerFlashlight.enabled = false;
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.L)) ToggleFlashlight();
+    }
+
+}
